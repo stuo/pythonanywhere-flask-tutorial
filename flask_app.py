7894723b11d31e6d2e947ab7d250979048667527
stuo <stuart.ormsby@talk21.com>
2016-06-01 23:02:49 +0000

    PythonAnywhere Flask Tutorial code

---
+++ flask_app.py
@@ -0,0 +1,39 @@
+
+# A very simple Flask Hello World app for you to get started with...
+
+from flask import Flask, redirect, render_template, request, url_for
+from flask.ext.sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
+app.config["DEBUG"] = True
+
+SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://{username}:{password}@{hostname}/{databasename}".format(
+    username="stuo",
+    password="mariners",
+    hostname="stuo.mysql.pythonanywhere-services.com",
+    databasename="stuo$comments",
+)
+app.config["SQLALCHEMY_DATABASE_URI"] = SQLALCHEMY_DATABASE_URI
+app.config["SQLALCHEMY_POOL_RECYCLE"] = 299
+
+db = SQLAlchemy(app)
+
+class Comment(db.Model):
+
+    __tablename__ = "comments"
+
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(4096))
+
+
+comments = []
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if request.method == "GET":
+        return render_template("main_page.html", comments=comments)
+
+    comments.append(request.form["contents"])
+    return redirect(url_for('index'))
+
